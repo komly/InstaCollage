7be82dfaabc5520c3802896cae8f10eca88f2356
Komly <komly@yandex.ru>
2014-09-05 17:08:15 +0400

    Add UI

--- InstaCollage/KMViewController.m
+++ InstaCollage/KMViewController.m
@@ -14,16 +14,14 @@
 
 @implementation KMViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UI Actions
+
+- (IBAction)makeCollageAction:(id)sender {
+    
 }
 
 @end
